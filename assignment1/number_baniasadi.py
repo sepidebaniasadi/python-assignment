be9756bbcc26ea0ae3937197c741c1573127f523
sepidebaniasadi <110469062+sepidebaniasadi@users.noreply.github.com>
2022-08-08 01:19:31 +0430

    Update number_baniasadi.py

--- assignment1/number_baniasadi.py
+++ assignment1/number_baniasadi.py
@@ -1,13 +1,8 @@
 from tempfile import TemporaryDirectory
 
 
-number=int(input("enter number please enter a number that has more than one digit:"))
-reverse=0
-while number > 0:
-  temp=number%10
-  reverse=(reverse*10)+temp
-  number=number//10
-digit5=(reverse%100000  )//10000
-digit2=(reverse%100)//10
+number=int(input("enter number please enter a number that has six digit:"))
+digit5=(number%100000  )//10000
+digit2=(number%100)//10
 sum=digit5+digit2
 print(sum)
