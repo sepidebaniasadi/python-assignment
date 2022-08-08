97f504b47328c3d4c3cbc517508f56e734a2aa2d
sepidebaniasadi <110469062+sepidebaniasadi@users.noreply.github.com>
2022-08-08 16:29:07 +0430

    Update cylinder_baniasai.py

--- assignment1/cylinder_baniasai.py
+++ assignment1/cylinder_baniasai.py
@@ -1,10 +1,9 @@
-
-height=int(input("enter hieght: "))
-Radius=int(input("enter radius: "))
+height=float(input("enter hieght: "))
+Radius=float(input("enter radius: "))
 pi=3.14
 side_area=2*pi*Radius*height
 volume=pi*Radius*Radius*height
-total_area=(2*pi*Radius*height)+(2*pi*Radius*Radius)
+total_area=(side_area)+(2*pi*Radius*Radius)
 print("side area is: ",side_area)
 print("volume is:",volume)
 print(" total area is : ",total_area)
