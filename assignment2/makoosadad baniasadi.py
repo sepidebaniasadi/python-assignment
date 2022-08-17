a1cf20257b63d83c082a3d9bbd2fea97d11c5a46
sepidebaniasadi <110469062+sepidebaniasadi@users.noreply.github.com>
2022-08-17 09:59:46 +0430

    Update makoosadad baniasadi.py

--- assignment2/makoosadad baniasadi.py
+++ assignment2/makoosadad baniasadi.py
@@ -1,11 +1,11 @@
-
 number=(int(input("enter a number: ")))
+firstnum=number
 reverse=0
 while(number>0): 
     temp=number%10
     reverse=((reverse*10)+temp)
     number=number//10
-if number==reverse:
+if firstnum==reverse:
     print( reverse,"matchwith your first number")
+else:
     print(reverse,"don't match with your first number")
-
