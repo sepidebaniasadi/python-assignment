6286f9ebd509b39469f198ef6c395c4616ea7a94
sepidebaniasadi <110469062+sepidebaniasadi@users.noreply.github.com>
2022-08-20 10:26:10 +0430

    Update bmi baniasadi.py

--- assignment2/bmi baniasadi.py
+++ assignment2/bmi baniasadi.py
@@ -1,17 +1,17 @@
 length=(float(input("enter your length with metr: ")))
 wieght=(float(input('enter your wieght with kg: ')))
 BMI=(wieght/(length**2))
-if BMI<=16 or BMI<18.5:
+if BMI>=16 and BMI<18.5:
     print(" your BMI is: ", BMI,'/n'"body wiegth deficit")
-elif BMI>=18.5 or BMI<24:
+elif BMI>=18.5 and BMI<24:
     print(" your BMI is: ", BMI,'/n'"normal")
-elif BMI>=24 or BMI<30:
+elif BMI>=24 and BMI<30:
     print(" your BMI is: ", BMI,'/n'"wieght over")  
-elif BMI>=30 or BMI<35:
+elif BMI>=30 and BMI<35:
     print(" your BMI is: ", BMI,'/n'"obesity first degree")     
-elif BMI>=35 or BMI<40:
+elif BMI>=35 and BMI<40:
     print(" your BMI is: ", BMI,'/n'"obesity second degree")  
-elif BMI>40:
+elif BMI>=40:
     print(" your BMI is: ", BMI,'/n'"obesity third degree")
 else:
     print("This interval was unpredictable.")
