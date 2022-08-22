9c145ced3873eec7c302ff2e3a6417e25328020b
sepidebaniasadi <110469062+sepidebaniasadi@users.noreply.github.com>
2022-08-22 09:46:12 +0430

    Update atm2.py

--- assignment3/atm2.py
+++ assignment3/atm2.py
@@ -1,8 +1,8 @@
 passwrd=['8','2','1','3']
 pass_p=['3','1','2','8']
 count=1
-while count<=4:
-    passu=list(input("enter password"))
+while count<4:
+    passu=list(input("enter password: "))
     if passwrd==passu:
           print('true')
           break
@@ -12,6 +12,9 @@ while count<=4:
     elif len(passu)>4:
         print("wrong you enter more digits")
         continue
+    elif len(passu)<4:
+            print("wrong you enter lower digits")
+            continue
     else:
       print('wrong')
       count+=1
