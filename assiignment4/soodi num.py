8222063043c4e3113bbff8b5c8f1224e0719d154
sepidebaniasadi <110469062+sepidebaniasadi@users.noreply.github.com>
2022-08-30 00:22:40 +0430

    Update soodi num.py

--- assiignment4/soodi num.py
+++ assiignment4/soodi num.py
@@ -1,4 +1,21 @@
-from ast import Continue
+#soodi motlaq
+#list=[]
+#listt=[]
+#for i in range (10):
+ #  list.append(num)
+#for i in range(len(list)):
+    #for j in range(0,len(list)-i-1):
+        #if list[j]>list[j+1]:
+            #list[j],list[j+1]=list[j+1],list[j]
+            #c=True
+    #if c==False:
+       # break
+#for i in range(len(list)):
+   # listt.append(list[i])
+#print('='*10,'sort','='*10,'\n',listt)
+
+
+#SOODI
 list=[]
 listt=[]
 for i in range (10):
