b309f10f3b0e1d88bcdc2581a127ab9a68533829
sepidebaniasadi <110469062+sepidebaniasadi@users.noreply.github.com>
2022-09-12 01:09:10 +0430

    Update def%&.py

--- assignment6/def%&.py
+++ assignment6/def%&.py
@@ -1,15 +1,42 @@
 
+# def dollara(m,n):
+#     for i in range(n):
+#         if i%2==0:  
+#             print("$&"*m)
+            
+            
+            
+#         else:
+#             print("&$"*m)
+            
+            
+           
+
+# n=int(input("Number of rows: "))
+# m=int(input("Number of columns: "))
+# print(dollara(m,n))
+
 def dollara(m,n):
     for i in range(n):
-        if i%2==0:  
-            print("$&"*m)
-            
-            
+        if i%2==0: 
+            if m%2==0: 
+              o=int(m/2)
+              print('$&'*o)
+            else:
+                o=int(m/2)+1
+                t=('$&'*o)
+                print(t[:-1])
+
             
         else:
-            print("&$"*m)
-            
-            
+            if m%2==0:
+                o=int(m/2)
+                print("&$"*o)
+            else:
+                o=int(m/2)+1
+                h=("&$"*o)
+                print(h[:-1])
+                   
            
 
 n=int(input("Number of rows: "))
